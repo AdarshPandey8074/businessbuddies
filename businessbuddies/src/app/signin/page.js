5e7e49eec215458470efe6ae5b11c8374e8ff407
Monarch <monarchk7@gmail.com>
2024-04-02 13:09:57 +0530

    initial commit

---
+++ businessbuddies/src/app/signin/page.js
@@ -0,0 +1,58 @@
+'use client'
+import React from "react";
+import signIn from "@/firebase/auth/signin";
+import { useRouter } from 'next/navigation'
+
+function Page() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const router = useRouter()
+
+    const handleForm = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signIn(email, password);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+        return router.push("/admin")
+    }
+    return (<div className="wrapper">
+
+
+        <section class="bg-gray-50 dark:bg-gray-900">
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create and account
+                        </h1>
+                        <form class="space-y-4 md:space-y-6" onSubmit={handleForm}>
+
+                            <div>
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+
+                            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">SIGN IN</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>);
+}
+
+export default Page;
