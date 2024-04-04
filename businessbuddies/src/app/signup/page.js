7758da3ec43fefe468b5d586798febcb33c9691f
Monarch <monarchk7@gmail.com>
2024-04-04 14:40:01 +0530

    updated files

--- businessbuddies/src/app/signup/page.js
+++ businessbuddies/src/app/signup/page.js
@@ -11,6 +11,10 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [acn, setAcnumber] = useState('');
     const [interest, setinterest] = useState('');
+    const [whatsappno, setWhatsappno] = useState('');
+    const [designation, setdesignation] = useState('');
+
+
     const [name, setName] = useState('');
     const [loading, setLoading] = useState(false);
     const router = useRouter();
@@ -26,6 +30,8 @@ const Signup = () => {
                 acn,
                 interest,
                 name,
+                whatsappno,
+                designation,
             });
             router.push('/dashboard');
         } catch (error) {
@@ -75,8 +81,16 @@ const Signup = () => {
                                     <input type="acnumber" name="acnumber" id="acnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456789" required="" onChange={(e) => setAcnumber(e.target.value)} />
                                 </div>
                                 <div>
-                                    <label for="interests" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your interests</label>
-                                    <input type="acnumber" name="acnumber" id="acnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456789" required="" onChange={(e) => setinterest(e.target.value)} />
+                                    <label for="interests" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Field</label>
+                                    <input type="acnumber" name="acnumber" id="acnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="business" required="" onChange={(e) => setinterest(e.target.value)} />
+                                </div>
+                                <div>
+                                    <label for="interests" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter your Whatsapp Number</label>
+                                    <input type="acnumber" name="acnumber" id="acnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456789" required="" onChange={(e) => setWhatsappno(e.target.value)} />
+                                </div>
+                                <div>
+                                    <label for="interests" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">What is your Designation</label>
+                                    <input type="acnumber" name="acnumber" id="acnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="student" required="" onChange={(e) => setdesignation(e.target.value)} />
                                 </div>
                                 <div>
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
